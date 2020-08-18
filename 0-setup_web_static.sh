f5e2de88db63d250d709317cf6bb97c48a9f6389
andrescallejasg <andres.callejasg@gmail.com>
2020-08-18 02:52:09 +0000

    Prepare  web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# script that sets up your web servers for the deployment of web_static
+apt-get -y update
+sudo apt-get -y install nginx
+mkdir /data/ /data/web_static/ /data/web_static/releases/
+mkdir /data/web_static/shared/ /data/web_static/releases/test/
+echo -e "<html>
+  <head>
+    <title>AirBnb Clone</title>
+  </head>
+  <body>
+    AirBnB Clone - Web Static
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+ln -fs /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/server_name _;/ location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
