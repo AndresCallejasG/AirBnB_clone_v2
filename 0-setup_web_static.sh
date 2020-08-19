b4e8d10654076db947eff84b7c19eeda12cbf8c2
andrescallejasg <andres.callejasg@gmail.com>
2020-08-19 22:11:29 +0000

    reverse commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,6 +2,7 @@
 # script that sets up your web servers for the deployment of web_static
 apt-get -y update
 sudo apt-get -y install nginx
+mkdir /data/ /data/web_static/ /data/web_static/releases/
 mkdir /data/web_static/shared/ /data/web_static/releases/test/
 echo -e "<html>
   <head>
@@ -13,5 +14,5 @@ echo -e "<html>
 </html>" > /data/web_static/releases/test/index.html
 ln -fs /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server_name _;/a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+sed -i '/server_name _;/a location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
